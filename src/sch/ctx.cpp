3c3736a09bf1b61df2f3b4defcc08ccf6d93f5a0
AmirFB <amirfakhimbabaei@gmail.com>
2023-03-06 17:21:27 -0500

    "addoperation" and "removeOperation" removed from Context.

---
+++ src/sch/ctx.cpp
@@ -0,0 +1,109 @@
+# include <ctx.h>
+
+# include <ctxd.h>
+# include <schd.h>
+
+# include <iostream>
+
+# include <cuda.h>
+# include <cudaTypedefs.h>
+# include <cuda_runtime.h>
+
+# include <torch/torch.h>
+
+using namespace FGPRS;
+
+using namespace std;
+using namespace torch;
+
+MyContext::MyContext(unsigned smCount, int index, bool isDefault)
+{
+	_default = isDefault;
+	this->smCount = smCount;
+	_pMutex = new mutex();
+	this->index = index;
+}
+
+bool MyContext::initialize()
+{
+	if (_default)
+		return true;
+
+	CUexecAffinityParam_v1 affinity;
+	affinity.type = CU_EXEC_AFFINITY_TYPE_SM_COUNT;
+	affinity.param.smCount.val = smCount;
+	auto result = cuCtxCreate_v3(&_context, &affinity, 1, 0, 0);
+
+	return result == CUDA_SUCCESS;
+}
+
+bool MyContext::select()
+{
+	if (_default)
+		return MyContext::selectDefault();
+
+	busy = true;
+
+	return cuCtxSetCurrent(_context) == CUDA_SUCCESS;
+}
+
+bool MyContext::selectDefault()
+{
+	return cuCtxSetCurrent(0) == CUDA_SUCCESS;
+}
+
+bool MyContext::release()
+{
+	busy = false;
+	return selectDefault();
+}
+
+bool MyContext::destroy()
+{
+	selectDefault();
+
+	if (_default)
+		return true;
+
+	return cuCtxDestroy(_context) == CUDA_SUCCESS;
+}
+
+void MyContext::lock()
+{
+	_pMutex->lock();
+}
+
+void MyContext::unlock()
+{
+	_pMutex->unlock();
+}
+
+void MyContext::addOperation(shared_ptr<Operation> operation)
+{
+	_changed = true;
+	_queue.push_back(operation);
+}
+
+void MyContext::removeOperation()
+{
+	_changed = true;
+	_queue.pop_front();
+}
+
+steady_clock::time_point MyContext::getFinishTime()
+{
+	if (_queue.size() == 0)
+		return steady_clock::now();
+
+	if (!_changed)
+		return _finishTime;
+
+	double sum = 0;
+
+	for (auto op : _queue)
+		sum += op->contextData[index].occupiedExecutionTime;
+
+	_changed = false;
+	_finishTime = _queue[0]->startTime + microseconds((int)sum);
+	return _finishTime;
+}
--- src/sch/ctx.h
+++ src/sch/ctx.h
@@ -1,9 +1,13 @@
 # ifndef CTX_H
 # define CTX_H
 
+# include <operation.h>
+
 # include <cstdint>
 # include <mutex>
 # include <iostream>
+# include <deque>
+# include <memory>
 
 # include <cuda.h>
 # include <cudaTypedefs.h>
@@ -18,11 +22,14 @@ namespace FGPRS
 		CUcontext _context;
 		bool _default;
 		mutable mutex* _pMutex;
+		deque<shared_ptr<Operation>> _queue;
+		bool _changed = true;
+		steady_clock::time_point _finishTime;
+
 
 	public:
 		bool busy = false;
 		unsigned smCount;
-		unsigned long queueDuration;
 		int index = -1;
 
 		MyContext() {}
@@ -34,36 +41,40 @@ namespace FGPRS
 		bool destroy();
 		void lock();
 		void unlock();
+
+		void addOperation(shared_ptr<Operation> operation);
+		void removeOperation();
+		steady_clock::time_point getFinishTime();
 	};
 
-	struct ContextData
-	{
-		MyContext* context;
-		double isolatedExecutionTime, occupiedExecutionTime;
-		int smCount;
+	// struct ContextData
+	// {
+	// 	MyContext* context;
+	// 	double isolatedExecutionTime, occupiedExecutionTime;
+	// 	int smCount;
 
-		ContextData(MyContext* context)
-		{
-			this->context = context;
-			smCount = context->smCount;
-			isolatedExecutionTime = 0;
-			occupiedExecutionTime = 0;
-		}
+	// 	ContextData(MyContext* context)
+	// 	{
+	// 		this->context = context;
+	// 		smCount = context->smCount;
+	// 		isolatedExecutionTime = 0;
+	// 		occupiedExecutionTime = 0;
+	// 	}
 
-		ContextData(MyContext* context, double isolatedExecutionTime, double occupiedExecutionTime)
-		{
-			this->context = context;
-			smCount = context->smCount;
-			this->isolatedExecutionTime = isolatedExecutionTime;
-			this->occupiedExecutionTime = occupiedExecutionTime;
-		}
+	// 	ContextData(MyContext* context, double isolatedExecutionTime, double occupiedExecutionTime)
+	// 	{
+	// 		this->context = context;
+	// 		smCount = context->smCount;
+	// 		this->isolatedExecutionTime = isolatedExecutionTime;
+	// 		this->occupiedExecutionTime = occupiedExecutionTime;
+	// 	}
 
-		void stackExecutionTime(ContextData ctxData)
-		{
-			this->isolatedExecutionTime += ctxData.isolatedExecutionTime;
-			this->occupiedExecutionTime += ctxData.occupiedExecutionTime;
-		}
-	};
+	// 	void stackExecutionTime(ContextData ctxData)
+	// 	{
+	// 		this->isolatedExecutionTime += ctxData.isolatedExecutionTime;
+	// 		this->occupiedExecutionTime += ctxData.occupiedExecutionTime;
+	// 	}
+	// };
 }
 
 # endif
