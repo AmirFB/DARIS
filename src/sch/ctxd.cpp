9fe05d6571da62565eeea1636cafbf61ba727a0d
AmirFB <amirfakhimbabaei@gmail.com>
2023-03-06 17:19:57 -0500

    "ContextData"'s declaration and implementation separated from "ctx.h" and added to "ctxd.h" and "ctxd.cpp".

---
+++ src/sch/ctxd.cpp
@@ -0,0 +1,27 @@
+# include <ctxd.h>
+
+# include <ctx.h>
+
+using namespace FGPRS;
+
+ContextData::ContextData(MyContext* context)
+{
+	this->context = context;
+	smCount = context->smCount;
+	isolatedExecutionTime = 0;
+	occupiedExecutionTime = 0;
+}
+
+ContextData::ContextData(MyContext* context, double isolatedExecutionTime, double occupiedExecutionTime)
+{
+	this->context = context;
+	smCount = context->smCount;
+	this->isolatedExecutionTime = isolatedExecutionTime;
+	this->occupiedExecutionTime = occupiedExecutionTime;
+}
+
+void ContextData::stackExecutionTime(ContextData ctxData)
+{
+	this->isolatedExecutionTime += ctxData.isolatedExecutionTime;
+	this->occupiedExecutionTime += ctxData.occupiedExecutionTime;
+}
---
+++ src/sch/ctxd.h
@@ -0,0 +1,30 @@
+# ifndef CTXD_H
+# define CTXD_H
+
+// # include <ctx.h>
+
+# include <cstdint>
+# include <mutex>
+# include <iostream>
+# include <memory>
+
+using namespace std;
+
+namespace FGPRS
+{
+	class MyContext;
+
+	class ContextData
+	{
+	public:
+		MyContext* context;
+		double isolatedExecutionTime, occupiedExecutionTime;
+		int smCount;
+
+		ContextData(MyContext* context);
+		ContextData(MyContext* context, double isolatedExecutionTime, double occupiedExecutionTime);
+		virtual void stackExecutionTime(ContextData ctxData);
+	};
+}
+
+# endif
